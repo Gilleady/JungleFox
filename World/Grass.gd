9baca5e0813a81c85d80e3f80d1d39ec32b5e3d7
Gilleady <79852665+Gilleady@users.noreply.github.com>
2024-03-25 21:28:59 -0300

    First Commit

---
+++ World/Grass.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
+var stats = PlayerStats
+
+func create_grass_effect():
+	var grassEffect = GrassEffect.instance()
+	get_parent().add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+func _on_Hurtbox_area_entered(_area):
+	create_grass_effect()
+	queue_free()
+	
+	var chance = randi() % 100 + 1
+	chance = chance % 2
+	if (chance == 0):
+		stats.health += 0.5
